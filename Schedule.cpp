8b594a62faeadacef73735c4afd78afe80e4fc91
Rodrigo de Sousa <up202205751@up.pt>
2023-10-30 18:12:57 +0000

    added Schedule

---
+++ Schedule.cpp
@@ -0,0 +1,5 @@
+//
+// Created by rodri on 27/10/23.
+//
+
+#include "Schedule.h"
---
+++ Schedule.h
@@ -0,0 +1,17 @@
+//
+// Created by rodri on 27/10/23.
+//
+
+#ifndef PROJECT1_SCHEDULE_H
+#define PROJECT1_SCHEDULE_H
+
+namespace uni {
+    class Schedule {
+    private
+    public:
+        void addClass()
+
+
+    };
+}
+#endif //PROJECT1_SCHEDULE_H
